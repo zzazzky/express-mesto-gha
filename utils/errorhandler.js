6e506a033e9eff4b654256ab2bb73f7b1afaee0b
Victoria Gerasimova <zzazzky@gmail.com>
2023-01-15 14:16:29 +0500

    fix: Исправлен некорректный статус у ошибки CastError

--- utils/errorhandler.js
+++ utils/errorhandler.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const { ValidationError } = mongoose.Error.ValidationError;
+const { CastError } = mongoose.Error.CastError;
 const NotFoundError = require('./NotFoundError');
 
 const handleError = (req, res) => {
   let ResStatus = 500;
-  if (req.err instanceof ValidationError) {
+  if (req.err instanceof ValidationError || CastError) {
     ResStatus = 400;
   } else if (req.err instanceof NotFoundError) {
     ResStatus = 404;
